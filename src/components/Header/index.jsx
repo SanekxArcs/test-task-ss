b042cd9904c3891719c49a66012f0b7fd852121a
Oleksandr Dzisiak <56607007+SanekxArcs@users.noreply.github.com>
2025-05-26 23:38:59 +0200

    feat: Add CartIcon and LogoFullIcon components

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import burgerMenu from "../../assets/icons/menu.svg";
 import crossMenu from "../../assets/icons/x-menu.svg";
-import logoFull from "../../assets/icons/logo-full.svg";
-import cartIcon from "../../assets/icons/cart.svg";
+import CartIcon from "../CartIcon";
+import LogoFullIcon from "../LogoFullIcon";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -11,29 +11,36 @@ const Header = () => {
     <header className="header">
       <div className="header__container">
         <div className="header__logo">
-          <a href="/">
-            <img src={logoFull} alt="Logo" />
+          <a href="/" aria-label="Logo earth go">
+            <LogoFullIcon
+              overGradient="hoverGradient"
+              className="header__logo-img"
+            />
           </a>
         </div>
 
-        <div className="header__menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
-          <img src={menuOpen ? crossMenu : burgerMenu} alt="Menu" />
-        </div>
-
-        <nav className={`header__links ${menuOpen ? "header__links--open" : ""}`}>
+        <nav
+          aria-label="Navigation"
+          className={`header__links ${menuOpen ? "header__links--open" : ""}`}
+        >
           <a href="/home" className="header__link">
             Home
           </a>
           <a href="/products" className="header__link">
             Products
           </a>
-          <a href="/cart" className="header__link">
-            <div className="cart-icon">
-              <img src={cartIcon} alt="Cart" />
-            </div>
+          <a href="/cart" aria-label="Cart" className="header__cart">
+            <CartIcon hoverGradient="hoverGradient" />
           </a>
-
         </nav>
+
+        <div
+          aria-label="Menu"
+          className="header__menu-icon"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <img src={menuOpen ? crossMenu : burgerMenu} alt="Menu" />
+        </div>
       </div>
     </header>
   );
